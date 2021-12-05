aff6aa5b336cc44dd97f86afe4030f9d8f1ad6d2
Shreecharanhegde <shreecharanhegde46948@gmail.com>
2021-12-05 22:30:43 +0530

    feat:added
    vgbgb

---
+++ app.js
@@ -0,0 +1,10 @@
+var btnTranslate = document.querySelector("#btn-translate")
+var textInput = document.querySelector("#txt-input")
+var outputDiv = document.querySelector("#output-area")
+
+
+function clickHandler() {
+    outputDiv.innerText = "tjbbuug" + textInput.ariaValueMax;}
+
+
+    btnTranslate.addEventListener("click", clickHandler)
